b3d7b9c71673796c8436c2ba38ded9235d19e7e6
Miles Kavanagh <kavana7@mail.nmc.edu>
2020-03-16 12:32:49 -0400

    push

--- TBGame.S3/Models/KeyItem.cs
+++ TBGame.S3/Models/KeyItem.cs
@@ -32,7 +32,7 @@ namespace TBGame.Models
         #region METHODS
         public override string InformationString()
         {
-            return $"{Name}\nValue: {Value}";
+            return $"{Name}";
         }
         #endregion
     }
